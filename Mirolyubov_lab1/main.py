bc120595c2edfa366cdbf3f2b85d63638edc72c8
ILYA <ilyamir1@mail.ru>
2023-03-31 16:00:43 +0300

    Improove lab2

--- Mirolyubov_lab1/main.py
+++ Mirolyubov_lab1/main.py
@@ -1,9 +1,10 @@
 from function import *
 
 if __name__ == "__main__":
-    hello_foo()
-    print(calculate(2, 3, "mult"))
     lst = list(range(10))
+    hello_foo()
+
+    print(calculate(2, 3, "mult"))
     print(lst)
     print(get_even_num(lst))
 
