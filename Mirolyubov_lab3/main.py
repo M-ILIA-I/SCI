44c2027645d49936a7f59419b0294cbb6abf3697
ILYA <ilyamir1@mail.ru>
2023-05-04 12:33:32 +0300

    final version lab3

--- Mirolyubov_lab3/main.py
+++ Mirolyubov_lab3/main.py
@@ -1,33 +1,38 @@
-import inspect
+import argparse
+from factory import Factory
 from json_serializer import Json
 from xml_serializer import Xml
-from test_value import *
-from packing import convert
+import test_value
 
-def summ(a=11, b=23):
-    return a+b
+if __name__ == '__main__':
+    # a = Xml()
+    # b = Json()
+    # with open('my_json_file', 'a') as f:
+    #     b.dump(test_value.Counter, f)
 
+    parser = argparse.ArgumentParser(description='Serializer of JSON, XML')
+    parser.add_argument('input_dir', type=str, help='Input dir')
+    parser.add_argument('source_format', type=str, help='Serialize format(json, xml) of source')
+    parser.add_argument('result_format', type=str, help='Serialize format(json, xml) for result')
+    parser.add_argument('output_dir', type=str, help='Output dir')
+    args = parser.parse_args()
 
-class Person(object):
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+    result_format = args.result_format
+    source_format = args.source_format
 
+    if source_format == result_format:
+        print("Same type of objects")
+        exit()
 
-def main():
-    ser = Json()
-    xm = Xml()
-    #print((xm.dumps(lambda_fanc)))
-    #a = xm.dumps({"b": {"c": {'d': {}}}, "A": 3})
-    #print(a)
-    #print(xm.loads(a))
-    #print(convert(lambda_fanc))
-    a = (xm.loads(xm.dumps(Employee)))("ilya", 300)
-    a.display_employee()
-     #print(xm._deserialize_from_str(xm.dumps(Counter)))
+    source_serializer = Factory.create_serializer(source_format)
+    result_serializer = Factory.create_serializer(result_format)
 
+    with open(args.input_dir) as file:
+        obj = source_serializer.load(file)
+        with open(args.output_dir, "w") as output_file:
+            result_serializer.dump(obj, output_file)
 
-
-
-if __name__ == "__main__":
-    main()
+    # with open('my_json_file') as file:
+    #     obj = b.load(file)
+    #     with open('my_xml_file', "w") as output_file:
+    #         a.dump(obj, output_file)
